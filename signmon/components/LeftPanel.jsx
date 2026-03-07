8efa605561966e3927573ce22f125a24b3c828ae
khlein_dev <kierjerymiepascua@gmail.com>
2026-03-07 22:28:51 +0800

    updated/ need some work

---
+++ signmon/components/LeftPanel.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function BottomPanel() {
+    return (
+        <View style={styles.leftPanel}>
+            <Text style={styles.panelText}>LEFT PANEL</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+
+    leftPanel: {
+        position: "absolute",
+        left: 0,
+        top: 0,
+        bottom: 0,
+        width: 200,
+        backgroundColor: "#222",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    panelText: {
+        color: "white",
+        fontWeight: "bold",
+    },
+});
